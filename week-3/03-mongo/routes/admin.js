1c46c3689690ba76e699fd0c243aad96fbd710f7
madhan999 <udayagirimadhan999@gmail.com>
2024-04-16 21:56:17 +0530

    adding

--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -5,6 +5,22 @@ const router = Router();
 // Admin Routes
 router.post('/signup', (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    Admin.create({
+        username,
+        password
+    })
+    .then(() => {
+        res.json({
+            message: 'Admin Created Successfully'
+        })
+    }).catch((err,message) => {
+        res.json({
+            message: 'Error in creating admin user.'
+        })
+    })
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
